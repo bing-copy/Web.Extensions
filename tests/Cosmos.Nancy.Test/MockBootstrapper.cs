1da1e930b44cdfcb0ce9b24c327b1bab7d243f87
alexinea <forerunner@outlook.com>
2017-05-30 21:21:27 +0800

    添加测试项目

---
+++ tests/Cosmos.Nancy.Test/MockBootstrapper.cs
@@ -0,0 +1,8 @@
+﻿using Nancy.Bootstrappers.Autofac;
+
+namespace Cosmos.Nancy.Test
+{
+    public class MockBootstrapper : AutofacNancyBootstrapper
+    {
+    }
+}
---
+++ tests/Cosmos.Nancy.Test/Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Cosmos.Nancy.Test.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            
+        }
+    }
+}
